f3dd8810f710af897722ac8da80143001d5aae59
Your Name <pip@gmail.com>
2023-03-15 20:08:54 +0300

    initial

---
+++ WebApplicationwhith.Test.Unit/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace WebApplicationwhith.Test.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var actual = 10;
+            Assert.Equal(10, actual);
+        }
+    }
+}
